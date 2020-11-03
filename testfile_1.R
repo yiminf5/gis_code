4c4b8e78ca5b544bdbbb2adbfd249d61f65bc89c
yiminf5 <ucfnfub@ucl.ac.uk>
2020-11-03 23:30:10 +0800

    test4

--- testfile_1.R
+++ testfile_1.R
@@ -9,3 +9,20 @@ q = x + y - z
 print(q)
 
 ## adding comment
+
+install.packages("reprex")
+
+library(reprex)
+
+reprex()
+
+``` r
+A <- 1
+B <- 2
+C <- A+B
+C
+#> [1] 3
+```
+
+<sup>Created on 2020-11-03 by the [reprex package](https://reprex.tidyverse.org) (v0.3.0)</sup>
+  
